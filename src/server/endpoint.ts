121568b5de39c22135de5223f291a816e522c2a6
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-20 21:35:56 +0900

    feat: エンドポイント`shutdown`を追加

--- src/server/endpoint.ts
+++ src/server/endpoint.ts
@@ -1,8 +1,10 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import devices from './endpoints/devices'
+import shutdown from './endpoints/shutdown'
 
 const endpoints: Record<string, (req: FastifyRequest, res: FastifyReply) => Promise<void>> = {
     '/devices': devices,
+    '/shutdown': shutdown,
 }
 
 export default endpoints
