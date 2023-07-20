36dedccbb26f95e209a39f8fbe79c521461f65a9
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-20 23:52:50 +0900

    update: メッセージを表示するように

--- src/server/endpoints/shutdown.ts
+++ src/server/endpoints/shutdown.ts
@@ -1,3 +1,6 @@
-export default async function () {
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+export default async function (req: FastifyRequest, res: FastifyReply) {
+    res.send('Shutting down...')
     process.exit(0)
 }
