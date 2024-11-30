06b4daad98a5968988d946f16f9d3c0aee905273
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-11-30 18:31:35 +0900

    fix: 終了前にレスポンスが送信できていなかった問題を修正

--- src/server/endpoints/shutdown.ts
+++ src/server/endpoints/shutdown.ts
@@ -1,6 +1,6 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 
 export default async function (req: FastifyRequest, res: FastifyReply) {
-    res.send('Shutting down...')
+    await res.send('Shutting down...')
     process.exit(0)
 }
